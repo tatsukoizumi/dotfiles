cd01d6c0d656ec604d64f80fbf8867f3ff3538dd
tatsukoizumi <koizumitattsuu@gmail.com>
2022-08-21 10:03:06 +0900

    Fix vscode path

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,7 +7,7 @@ paths=($VSCODE_SETTING_DIR/keybindings.json $VSCODE_SETTING_DIR/settings.json)
 
 for file_path in ${paths[@]}
 do
-    ln -snfv $file_path ~/Library/Application\ Support/Code/User
+    ln -snfv $file_path $HOME/Library/Application\ Support/Code/User
 done
 
 cat $VSCODE_SETTING_DIR/extensions | while read line
