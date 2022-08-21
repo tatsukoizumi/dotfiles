e96ccd60366b7f9b90e3d956fad8e1e6ab45abfb
Koizumi Tatsunori <tatsukoizumi@KoizuminoMacBook-Pro-3.local>
2022-08-21 11:52:14 +0900

    Use home path

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,14 +3,12 @@ set -eu
 
 paths=(keybindings.json settings.json)
 
-cd $HOME
-
 for file_path in ${paths[@]}
 do
-    ln -snfv ./dotfiles/vscode/$file_path ./Library/Application\ Support/Code/User/$file_path
+    ln -snfv $HOME/dotfiles/vscode/$file_path $HOME/Library/Application\ Support/Code/User/$file_path
 done
 
-cat ./dotfiles/vscode/extensions | while read line
+cat $HOME/dotfiles/vscode/extensions | while read line
 do
     code --install-extension $line
 done
