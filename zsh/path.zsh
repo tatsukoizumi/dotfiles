172bee32f6317f56886529574f3fa74f78094b3c
tatsukoizumi <koizumitattsuu@gmail.com>
2022-11-19 12:25:05 +0900

    add ghc path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,5 +2,7 @@
 export PATH="$HOME/.nodenv/bin:$PATH"
 # curl
 export PATH="/usr/local/opt/curl/bin:$PATH"
+#[ -f "/Users/tatsukoizumi/.ghcup/env" ] && source "/Users/tatsukoizumi/.ghcup/env" # ghcup-env
+[ -f "/Users/tatsukoizumi/.ghcup/env" ] && source "/Users/tatsukoizumi/.ghcup/env" # ghcup-env
 
 eval "$(nodenv init -)"
