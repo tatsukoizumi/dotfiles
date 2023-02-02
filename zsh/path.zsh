4708d788eea9cf0534927b70133e746e1242005f
tatsunori_koizumi <koizumitattsuu@gmail.com>
2023-02-02 15:14:47 +0900

    add GO path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,3 +6,6 @@ export PATH="/usr/local/opt/curl/bin:$PATH"
 [ -f "/Users/tatsukoizumi/.ghcup/env" ] && source "/Users/tatsukoizumi/.ghcup/env" # ghcup-env
 
 eval "$(nodenv init -)"
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
