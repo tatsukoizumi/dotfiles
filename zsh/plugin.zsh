161508c6e40b3cfda529e3904caffe6f55acc9ca
tatsukoizumi <koizumitattsuu@gmail.com>
2023-05-25 08:00:09 +0900

    fix plugin

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -6,16 +6,15 @@ zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "mrowa44/emojify", as:command
-zplug "romkatv/powerlevel10k", as:theme, depth:1
 zplug "b4b4r07/enhancd", use:init.sh
 
 # Install plugins if not installed
 if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo
-        zplug install
-    fi
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo
+    zplug install
+  fi
 fi
 
 zplug load
