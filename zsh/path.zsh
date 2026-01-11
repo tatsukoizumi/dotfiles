3e2abfbed8c4fadf75404df127881d5b1277181e
tatsukoizumi <koizumitattsuu@gmail.com>
2026-01-11 11:06:21 +0900

    update path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,3 +5,12 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=/opt/homebrew/bin:$PATH
 export PATH="$HOME/.local/share/mise/shims:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
+# pnpm
+export PNPM_HOME="/Users/koizumitatsunori/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
